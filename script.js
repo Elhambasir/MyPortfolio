ee0fff93357e3b0d1cd47a3855eddd0e1c38c669
Nelofar Zabi <Nelofarzabi2000@gmail.com>
2023-03-06 06:43:50 -0800

    Add function to JS file

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const nav = document.querySelector('.toolbar');
 const headline = document.querySelector('.headline');
 const btn_menu = document.querySelector('.btn-menu')
 const btn_close = document.querySelector('.btn-close');
+const btn_menu_item1 = document.querySelector('.btn-menu-item1');
+const btn_menu_item2 = document.querySelector('.btn-menu-item2');
+const btn_menu_item3 = document.querySelector('.btn-menu-item3');
 
 btn_menu.onclick = function show() {
   nav.classList.add('display');
@@ -9,5 +12,14 @@ btn_menu.onclick = function show() {
 };
 
 btn_close.onclick = function show() {
+  nav.classList.remove('display');
+};
+btn_menu_item1.onclick = function show() {
+  nav.classList.remove('display');
+};
+btn_menu_item2.onclick = function show() {
+  nav.classList.remove('display');
+};
+btn_menu_item3.onclick = function show() {
   nav.classList.remove('display');
 };
