39b2d142c9059c8b89cf5061347f177048f7008c
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-03-06 18:20:59 +0430

    Add mobile menu

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn_menu = document.querySelector('.collapse-button');
-const nav = document.querySelector('.toolbar');
-
-btn_menu.onclick = function show() {
-  nav.classList.toggle('.display');
-};
