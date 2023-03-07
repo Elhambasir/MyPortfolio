7e470e3466c06b03fed46769da1a6a71bd3a5906
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-03-07 10:54:59 +0430

    Edit linter file

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const nav = document.querySelector('.toolbar');
 const headline = document.querySelector('.headline');
-const btn_menu = document.querySelector('.btn-menu')
-const btn_close = document.querySelector('.btn-close');
-const btn_menu_item1 = document.querySelector('.btn-menu-item1');
-const btn_menu_item2 = document.querySelector('.btn-menu-item2');
-const btn_menu_item3 = document.querySelector('.btn-menu-item3');
+const btnMenu = document.querySelector('.btn-menu')
+const btnClose = document.querySelector('.btn-close');
+const btnMenuItem1 = document.querySelector('.btn-menu-item1');
+const btnMenuItem2 = document.querySelector('.btn-menu-item2');
+const btnMenuItem3 = document.querySelector('.btn-menu-item3');
 
-btn_menu.onclick = function show() {
+btnMenu.onclick = function show() {
   nav.classList.add('display');
   headline.classList.add('position');
 };
 
-btn_close.onclick = function show() {
+btnClose.onclick = function show() {
   nav.classList.remove('display');
 };
-btn_menu_item1.onclick = function show() {
+btnMenu_item1.onclick = function show() {
   nav.classList.remove('display');
 };
-btn_menu_item2.onclick = function show() {
+btnMenu_item2.onclick = function show() {
   nav.classList.remove('display');
 };
-btn_menu_item3.onclick = function show() {
+btnMenu_item3.onclick = function show() {
   nav.classList.remove('display');
 };
