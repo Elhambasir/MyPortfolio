e34243d8f32c6e4582b2e79f901d9b6f167262c5
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-03-09 15:15:19 +0430

    Add email validation to contact form

--- script.js
+++ script.js
@@ -30,3 +30,20 @@ btnMenuItem2.onclick = function show() {
 btnMenuItem3.onclick = function show() {
   nav.classList.remove('display');
 };
+
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const emailError = document.querySelector('.btnSubmit + small.error');
+const emailRegExp = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+
+email.addEventListener('input', (event) => {
+
+  if (!emailRegExp.test(email.value)) {
+    emailError.innerHTML = 'Email must be in lowercase';
+    document.querySelector('.btnSubmit').type = 'button';
+  }
+  if (email.value == email.value.toLowerCase()) {
+    emailError.innerHTML = '';
+    document.querySelector('.btnSubmit').type = 'submit';
+  }
+});
