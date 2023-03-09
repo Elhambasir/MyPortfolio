870728bf052ecb0f91d5b8f72fbaf892ba80621d
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-03-09 12:41:22 +0430

    Fix error

--- script.js
+++ script.js
@@ -32,92 +32,90 @@ btnMenuItem3.onclick = function show() {
 };
 
 // ------Projects--------
-let projectInfo = [
+const projectInfo = [
   {
-    id: "p1",
-    title: "Preect-1",
-    img: "./assets/images/Snapshoot Portfolio.png",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint veniam perspiciatis voluptas"
-      + " asperiores assumenda, ducimus, sapiente sequi iure debitis rem consequuntur necessitatibus adipisci.",
-    labels: ["CANOPY", "Back End Dev", 1995],
-    tags: ["html", "css", "javascript"]
+    id: 'p1',
+    title: 'Preect-1',
+    img: './assets/images/Snapshoot Portfolio.png',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint veniam perspiciatis voluptas'
+      + ' asperiores assumenda, ducimus, sapiente sequi iure debitis rem consequuntur necessitatibus adipisci.',
+    labels: ['CANOPY', 'Back End Dev', 1995],
+    tags: ['html', 'css', 'javascript'],
   },
   {
-    id: "p2",
-    title: "Preect-2",
-    img: "./assets/images/2.png",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint veniam perspiciatis voluptas"
-      + " asperiores assumenda, ducimus, sapiente sequi iure debitis rem consequuntur necessitatibus adipisci.",
-    labels: ["CANOPY", "Back End Dev", 2015],
-    tags: ["html", "css", "javascript"]
+    id: 'p2',
+    title: 'Preect-2',
+    img: './assets/images/2.png',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint veniam perspiciatis voluptas'
+      + ' asperiores assumenda, ducimus, sapiente sequi iure debitis rem consequuntur necessitatibus adipisci.',
+    labels: ['CANOPY', 'Back End Dev', 2015],
+    tags: ['html', 'css', 'javascript'],
   },
   {
-    id: "p3",
-    title: "Preect-3",
-    img: "./assets/images/1.png",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint veniam perspiciatis voluptas"
-      + " asperiores assumenda, ducimus, sapiente sequi iure debitis rem consequuntur necessitatibus adipisci.",
-    labels: ["CANOPY", "Back End Dev", 2015],
-    tags: ["html", "css", "javascript"]
-  }
+    id: 'p3',
+    title: 'Preect-3',
+    img: './assets/images/1.png',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint veniam perspiciatis voluptas'
+      + ' asperiores assumenda, ducimus, sapiente sequi iure debitis rem consequuntur necessitatibus adipisci.',
+    labels: ['CANOPY', 'Back End Dev', 2015],
+    tags: ['html', 'css', 'javascript'],
+  },
 ];
-
 
 const section = document.querySelector('.projects');
 
+projectInfo.forEach((element) => {
+  const tags = document.createElement('ul');
+  const labels = document.createElement('ul');
+  const desc = document.createElement('p');
+  const image = document.createElement('img');
+  const title = document.createElement('h1');
+  const imagCover = document.createElement('div');
+  const projectDetail = document.createElement('div');
+  const card = document.createElement('div');
+  const btn = document.createElement('button');
+  const btnP = document.createElement('p');
 
-projectInfo.forEach((element, index) => {
-  let tags = document.createElement('ul');
-  let labels = document.createElement('ul');
-  let desc = document.createElement('p');
-  let image = document.createElement('img');
-  let title = document.createElement('h1');
-  let imagCover = document.createElement('div');
-  let projectDetail = document.createElement('div');
-  let card = document.createElement('div');
-  let btn = document.createElement('button');
-  let btnP = document.createElement('p');
-
-  element.tags.forEach((tagElement, index) => {
-    let tagItem = document.createElement('li');
-    let liItem = document.createElement('a');
-    liItem.href = "#";
-    let tagItemContent = document.createTextNode(tagElement);
+  element.tags.forEach((tagElement) => {
+    const tagItem = document.createElement('li');
+    const liItem = document.createElement('a');
+    liItem.href = '#';
+    const tagItemContent = document.createTextNode(tagElement);
     liItem.appendChild(tagItemContent);
     tagItem.appendChild(liItem);
     tags.appendChild(tagItem);
-  })
+  });
 
-  element.labels.forEach((labelElement, index) => {
-    let labelItem = document.createElement('li');
-    let labelItemContent = document.createTextNode(labelElement);
+  element.labels.forEach((labelElement) => {
+    const labelItem = document.createElement('li');
+    const labelItemContent = document.createTextNode(labelElement);
     labelItem.appendChild(labelItemContent);
     labels.appendChild(labelItem);
-  })
+  });
   image.src = element.img;
   imagCover.appendChild(image);
-  let titleText = document.createTextNode(element.title);
+  const titleText = document.createTextNode(element.title);
   title.appendChild(titleText);
   projectDetail.appendChild(title);
   projectDetail.appendChild(labels);
   desc.innerText = element.description;
   projectDetail.appendChild(desc);
   projectDetail.appendChild(tags);
-  btn.innerHTML = "<a href='#detail'>See Project</a>";
+  btn.innerHTML = '<a href="#detail">See Project</a>';
   btnP.appendChild(btn);
   projectDetail.appendChild(btnP);
   card.appendChild(imagCover);
-  card.appendChild(projectDetail)
+  card.appendChild(projectDetail);
   section.appendChild(card);
-  card.classList = "card-works";
-  imagCover.classList = "image-cover order-1";
-  projectDetail.classList = "project-info";
-  labels.classList = "label";
-  tags.classList = "tags"
+  card.classList = 'card-works';
+  imagCover.classList = 'image-cover order-1';
+  projectDetail.classList = 'project-info';
+  labels.classList = 'label';
+  tags.classList = 'tags'
   //...........................   
   btn.addEventListener('click', function () {
-    let popup_card = document.querySelector('#detail');
-    projectInfo.forEach((item, index) => {
+    const popup_card = document.querySelector('#detail');
+    projectInfo.forEach((item) => {
       if (item.title === btn.parentNode.parentElement.firstChild.textContent) {
         popup_card.children[1].children[0].children[0].innerHTML = item.title;
 
@@ -134,15 +132,15 @@ projectInfo.forEach((element, index) => {
       }
     })
     popup_card.classList.add('show');
-    let conttainer = document.querySelector('.container');
+    const conttainer = document.querySelector('.container');
     conttainer.classList.add('blur');
-  })
-})
-let btnCloseProject = document.querySelector('#closeProject');
+  });
+});
+const btnCloseProject = document.querySelector('#closeProject');
 btnCloseProject.addEventListener('click', function () {
-  let popup_card = document.querySelector('#detail');
+  const popup_card = document.querySelector('#detail');
   popup_card.classList.remove('show');
-  let conttainer = document.querySelector('.container');
+  const conttainer = document.querySelector('.container');
   conttainer.classList.remove('blur');
 })
 
